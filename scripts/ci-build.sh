9a166cd970f81c7993ee991dd6e6b102a027a7e0
Gunnar Andersson <gandersson@genivi.org>
2018-04-25 10:21:18 +0200

    ci-build: Change default for reused (fixed) directories to false
    There are some "fixed" locations for caching (downloads, sstate) used for
    all builds by build agents.  These used to be used by default, and could
    be overridden to not be used.  The logic is now reveresed to be better
    optimized for end-user usage, and less optimized for CI.
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -251,8 +251,8 @@ define_with_default CREATE_RELEASE_DIR false
 define_with_default MIRROR "https://docs.projects.genivi.org/releases/yocto_mirror"
 define_with_default PREMIRROR ""  # By default none (but we have the shared DL_DIR)
 define_with_default RM_WORK false
-define_with_default REUSE_STANDARD_DL_DIR true
-define_with_default REUSE_STANDARD_SSTATE_DIR true
+define_with_default REUSE_STANDARD_DL_DIR false
+define_with_default REUSE_STANDARD_SSTATE_DIR false
 define_with_default SGX_DRIVERS $AGENT_STANDARD_SGX_LOCATION
 define_with_default SGX_GEN_3_DRIVERS $AGENT_STANDARD_SGX_GEN3_LOCATION
 define_with_default SOURCE_ARCHIVE false
